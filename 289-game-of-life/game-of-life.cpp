07266da1b1f17d4786c162069235b49d9d7b531c
Marc Maravall Díez <marcmaravall@gmail.com>
2026-04-24 19:53:47 +0200

    Time: 0 ms (100.00%) | Memory: 9.6 MB (62.49%) - LeetSync

---
+++ 289-game-of-life/game-of-life.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    void gameOfLife(vector<vector<int>>& board) {
+        int n = board.size();
+        int m = board[0].size();
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                int neighbors = 0;
+                if (i != 0 && board[i-1][j] % 2 == 1)
+                    neighbors++;
+                if (i != n-1 && board[i+1][j] % 2 == 1)
+                    neighbors++;
+                if (j != 0 && board[i][j-1] % 2 == 1)
+                    neighbors++;
+                if (j != m-1 && board[i][j+1] % 2 == 1)
+                    neighbors++;
+                    
+                if (i != 0 && j != 0 && board[i-1][j-1] % 2 == 1)
+                    neighbors++;
+                if (i != n-1 && j != 0 && board[i+1][j-1] % 2 == 1)
+                    neighbors++;
+                if (i != 0 && j != m-1 && board[i-1][j+1] % 2 == 1)
+                    neighbors++;
+                if (i != n-1 && j != m-1 && board[i+1][j+1] % 2 == 1)
+                    neighbors++;
+
+                // 
+                bool living = board[i][j] % 2 == 1;
+
+                if (neighbors < 2 && living)
+                    board[i][j] = 1;
+                else if (neighbors <= 3 && living)
+                    board[i][j] = 3;
+                else if (neighbors > 3 && living)
+                    board[i][j] = 1;
+                else if (neighbors == 3 && !living)
+                    board[i][j] = 2;
+                else if (!living)
+                    board[i][j] = 0;
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                board[i][j] = board[i][j] >= 2 ? 1 : 0;
+            }
+        }
+    }
+};
