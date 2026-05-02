e18bbf4453e21660d1ecfe56cec578ac4a2c3122
Marc Maravall Díez <marcmaravall@gmail.com>
2026-05-02 22:29:34 +0200

    Time: 0 ms (100.00%) | Memory: 8.9 MB (66.84%) - LeetSync

--- 768-partition-labels/partition-labels.cpp
+++ 768-partition-labels/partition-labels.cpp
@@ -2,24 +2,22 @@ class Solution {
 public:
     vector<int> partitionLabels(string s) {
         int n = s.size();
-        std::vector<int> lastIndex(127, 0);
-
+        std::vector<int> map(127, 0);
         for (int i = 0; i < n; i++) {
-            lastIndex[s[i]] = i;
+            map[s[i]] = i;
         }
 
-        std::vector<int> res = {};
+        std::vector<int> res;
         int start = -1;
         int end = 0;
+
         for (int i = 0; i < n; i++) {
-            char c = s[i];
-            end = max(end, lastIndex[c]);
+            end = max(end, map[s[i]]);
             if (end == i) {
                 res.push_back(end-start);
                 start = end;
             }
         }
-
         return res;
     }
 };
