40df3b37cef7023ce93074d6828e7c5b0eeafcfb
Marc Maravall Díez <marcmaravall@gmail.com>
2026-05-03 20:10:59 +0200

    Time: 7 ms (7.10%) | Memory: 21.6 MB (74.57%) - LeetSync

---
+++ 57-insert-interval/insert-interval.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+        intervals.push_back(newInterval);
+        std::sort(intervals.begin(), intervals.end());
+
+        int n = intervals.size();
+        std::vector<std::vector<int>> res;
+        for (int i = 0; i < n; i++) {
+            int start = intervals[i][0];
+            if (i >= n-1) {
+                res.push_back({start, intervals[i][1]});
+                break;
+            }
+
+            int end = intervals[i][1];
+            while (i < n-1 && end >= intervals[i+1][0]) {
+                end = max(end, intervals[i+1][1]);
+                i++;
+            }
+
+            res.push_back({start, end});
+        }
+
+        return res;
+    }
+};
