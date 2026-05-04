9c3be81c340677e6ed8822507b5aeaa6f3662d2b
Marc Maravall Díez <marcmaravall@gmail.com>
2026-05-04 21:10:16 +0200

    Time: 23 ms (92.18%) | Memory: 64.9 MB (87.04%) - LeetSync

--- 1014-k-closest-points-to-origin/k-closest-points-to-origin.cpp
+++ 1014-k-closest-points-to-origin/k-closest-points-to-origin.cpp
@@ -3,7 +3,7 @@ public:
     vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
         std::vector<std::vector<int>> res;
         std::sort(points.begin(), points.end(), [](const std::vector<int>& a, const std::vector<int>& b) {
-            return sqrt(a[0]*a[0] + a[1]*a[1]) < sqrt(b[0]*b[0] + b[1]*b[1]);
+            return (a[0]*a[0] + a[1]*a[1]) < (b[0]*b[0] + b[1]*b[1]);
         });
 
         for (int i = 0; i < k; i++) {
