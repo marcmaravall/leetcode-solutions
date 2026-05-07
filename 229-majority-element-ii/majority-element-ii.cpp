6758818582d15d62cd53a51f36435d0f75d9af73
Marc Maravall Díez <marcmaravall@gmail.com>
2026-05-07 07:56:22 +0200

    Time: 0 ms (100.00%) | Memory: 23.7 MB (63.74%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.cpp
+++ 229-majority-element-ii/majority-element-ii.cpp
@@ -2,15 +2,43 @@ class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
         int n = nums.size();
-        std::unordered_map<int, int> freq;
-        std::vector<int> res;
-        for (int x : nums) {
-            freq[x]++;
-            if (freq[x] > n/3) {
-                res.push_back(x);
-                freq[x] = INT_MIN;
+
+        int candidate1 = 0;
+        int count1 = 0;
+        int candidate2 = 0;
+        int count2 = 0;
+        for (int num : nums) {
+            if(num == candidate1){
+                count1++;
+            } else if(num == candidate2){
+                count2++;
+            } else if(count1 == 0){
+                candidate1 = num;
+                count1 = 1;
+            } else if(count2 == 0) {
+                candidate2 = num;
+                count2 = 1;
+            } else {
+                count1--;
+                count2--;
             }
         }
+
+        count1 = 0;
+        count2 = 0;
+        for (int num : nums) {
+            if (num == candidate1)
+                count1++;
+            else if (num == candidate2)
+                count2++;
+        }
+
+        std::vector<int> res;
+        if(count1 > n/3) 
+            res.push_back(candidate1);
+        if(count2 > n/3) 
+            res.push_back(candidate2);
+
         return res;
     }
 };
