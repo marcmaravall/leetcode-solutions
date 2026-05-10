d43eb9f58f2eccd693275b009250382b8f9e41b9
Marc Maravall Díez <marcmaravall@gmail.com>
2026-05-10 22:12:33 +0200

    Time: 42 ms (5.44%) | Memory: 80.4 MB (48.93%) - LeetSync

---
+++ 2497-maximum-matching-of-players-with-trainers/maximum-matching-of-players-with-trainers.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int matchPlayersAndTrainers(vector<int>& g, vector<int>& s) {
+        std::sort(g.begin(), g.end());
+        std::sort(s.begin(), s.end());
+    
+        int res = 0;
+
+        for (int i = 0, j= 0; i < g.size() && j < s.size(); (void)0) {
+            if (s[j] >= g[i]) {
+                res++;
+                i++;
+            }
+            j++;
+        }
+
+        return res;
+    }
+};
