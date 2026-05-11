0b73f9acf24b59ab3b02dd0f6c60c46fe026baea
Marc Maravall Díez <marcmaravall@gmail.com>
2026-05-11 10:16:21 +0200

    Time: 2 ms (61.42%) | Memory: 15.2 MB (68.87%) - LeetSync

--- 2639-separate-the-digits-in-an-array/separate-the-digits-in-an-array.cpp
+++ 2639-separate-the-digits-in-an-array/separate-the-digits-in-an-array.cpp
@@ -3,17 +3,11 @@ public:
     vector<int> separateDigits(vector<int>& nums) {
         std::vector<int> res;
         for (int n : nums) {
-            std::vector<int>a;
-            while (n > 9) {
-                int d = n%10;
-                a.push_back(d);
-                n/=10;
-            }
-            a.push_back(n);
-            std::reverse(a.begin(), a.end());
-            res.insert(res.end(), a.begin(), a.end());
+            std::string str = std::to_string(n);
+            for (char c : str)
+                res.push_back(c-'0');
         }
-        
+
         return res;
     }
 };
