957d94acfad3f08587b6aa6e2e5ff489d90920b4
Marc Maravall Díez <marcmaravall@gmail.com>
2026-05-12 22:27:11 +0200

    Time: 29 ms (87.92%) | Memory: 97.4 MB (62.32%) - LeetSync

--- 1784-minimum-initial-energy-to-finish-tasks/minimum-initial-energy-to-finish-tasks.cpp
+++ 1784-minimum-initial-energy-to-finish-tasks/minimum-initial-energy-to-finish-tasks.cpp
@@ -4,8 +4,10 @@ public:
         std::sort(tasks.begin(), tasks.end(), [](std::vector<int>& a, std::vector<int>& b) {
             return (a[1]-a[0]) > (b[1]-b[0]);
         });
+
         int res = 0;
         int cost = 0;
+        
         for (std::vector<int>& task : tasks) {
             res += cost > task[1] ? 0 : task[1] - cost;
             cost = max(task[1] - task[0], cost - task[0]);
