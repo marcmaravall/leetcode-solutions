60d32a79b694240dc3064bef70671e4cf3c84560
Marc Maravall Díez <marcmaravall@gmail.com>
2026-05-13 22:03:18 +0200

    Time: 3 ms (74.09%) | Memory: 48.6 MB (62.10%) - LeetSync

--- 442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.cpp
+++ 442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.cpp
@@ -4,9 +4,12 @@ public:
         std::unordered_map<int, int> map;
         std::vector<int> res;
         for (int n : nums) {
-            map[n]++;
-            if (map[n] == 2)
-                res.push_back(n);
+            int i = abs(n) - 1;
+            if (nums[i] < 0) {
+                res.push_back(abs(n));
+            } else {
+                nums[i] = -nums[i];
+            }
         }
         return res;
     }
