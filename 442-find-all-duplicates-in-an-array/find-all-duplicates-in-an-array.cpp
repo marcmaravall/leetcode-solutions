ccb4818dde58cb388881b876f0a59958143ca78a
Marc Maravall Díez <marcmaravall@gmail.com>
2026-05-13 22:10:54 +0200

    Time: 0 ms (100.00%) | Memory: 48.4 MB (81.96%) - LeetSync

--- 442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.cpp
+++ 442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     vector<int> findDuplicates(vector<int>& nums) {
-        std::unordered_map<int, int> map;
         std::vector<int> res;
+        
         for (int n : nums) {
             int i = abs(n) - 1;
             if (nums[i] < 0) {
