fdeeeb2425131395f0e179bf9973c502b97d559e
Marc Maravall Díez <marcmaravall@gmail.com>
2026-05-14 11:45:47 +0200

    Time: 0 ms (100.00%) | Memory: 13.9 MB (55.42%) - LeetSync

---
+++ 260-single-number-iii/single-number-iii.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> singleNumber(vector<int>& nums) {
+        std::vector<int> res;
+        std::sort(nums.begin(), nums.end());
+
+        size_t n = nums.size();
+        for (int i = 0; i < n; i++) {
+            if (i == n-1) {
+                if (nums[i-1] != nums[i])
+                    res.push_back(nums[i]);
+            }
+            else if (i == 0) {
+                if (nums[i+1] != nums[i])
+                    res.push_back(nums[i]);
+            }
+            else if (nums[i+1] != nums[i] && nums[i-1] != nums[i]) {
+                res.push_back(nums[i]);
+            }
+        }
+
+        return res;
+    }
+};
