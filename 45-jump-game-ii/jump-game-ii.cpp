80168fdea4497927ea219346dab982bce9240fdb
Marc Maravall Díez <marcmaravall@gmail.com>
2026-05-17 15:44:15 +0200

    Time: 0 ms (100.00%) | Memory: 20.3 MB (82.90%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.cpp
+++ 45-jump-game-ii/jump-game-ii.cpp
@@ -5,7 +5,6 @@ public:
         int res = 0;
         int end = 0;
         int far = 0;
-
         for (int i = 0; i < n-1; i++) {
             far = max(far, i + nums[i]);
             if (i == end) {
