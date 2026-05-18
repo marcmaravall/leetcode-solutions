2fab99121107472be11bf8890dedf6964260d0d0
Marc Maravall Díez <marcmaravall@gmail.com>
2026-05-18 20:15:26 +0200

    Time: 4 ms (3.46%) | Memory: 24.6 MB (7.59%) - LeetSync

--- 3221-find-the-peaks/find-the-peaks.cpp
+++ 3221-find-the-peaks/find-the-peaks.cpp
@@ -3,11 +3,13 @@ public:
     vector<int> findPeaks(vector<int>& mountain) {
         std::vector<int> res;
         const int n = mountain.size();
+
         for (int i = 1; i < n-1; i++) {
             if (mountain[i-1] < mountain[i] && mountain[i] > mountain[i+1]) {
                 res.push_back(i);
             }
         }
+        
         return res;
     }
 };
