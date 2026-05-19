0e6dcf01a99c21216789652aa1759f61c16bffe8
Marc Maravall Díez <marcmaravall@gmail.com>
2026-05-19 10:59:46 +0200

    Time: 0 ms (100.00%) | Memory: 54.7 MB (30.11%) - LeetSync

--- 2634-minimum-common-value/minimum-common-value.cpp
+++ 2634-minimum-common-value/minimum-common-value.cpp
@@ -3,14 +3,14 @@ public:
     int getCommon(vector<int>& nums1, vector<int>& nums2) {
         const int n1 = nums1.size();
         const int n2 = nums2.size();
+        
         int i = 0, j = 0;
+
         while (i < n1 && j < n2) {
-            if (nums1[i] < nums2[j]) {
+            if (nums1[i] < nums2[j]) 
                 i++;
-            }
-            else if (nums2[j] < nums1[i]) {
+            else if (nums2[j] < nums1[i]) 
                 j++;
-            }
             
             if (i < n1 && j < n2 && nums1[i] == nums2[j])
                 return nums1[i];
