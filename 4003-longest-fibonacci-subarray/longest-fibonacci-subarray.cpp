6830a762a0f78d0242ba5f8acca47a2cf1d8688f
Marc Maravall Díez <marcmaravall@gmail.com>
2026-05-21 20:43:29 +0200

    Time: 3 ms (43.88%) | Memory: 102.6 MB (93.35%) - LeetSync

--- 4003-longest-fibonacci-subarray/longest-fibonacci-subarray.cpp
+++ 4003-longest-fibonacci-subarray/longest-fibonacci-subarray.cpp
@@ -3,14 +3,17 @@ public:
     int longestSubarray(vector<int>& nums) {
         int res = 2;
         const int n = nums.size();
+
         for (int i = 2; i < n; i++) {
             int current = 2;
             while (i < n && nums[i-2] + nums[i-1] == nums[i]) {
                 current++;
                 i++;
             }
+        
             res = max(res, current);
         }
+
         return res;
     }
 };
