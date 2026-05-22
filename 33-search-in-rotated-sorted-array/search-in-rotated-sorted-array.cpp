5251aed39eeccb786520f59fdbc1e37b92ad0a4b
Marc Maravall Díez <marcmaravall@gmail.com>
2026-05-22 11:55:29 +0200

    Time: 0 ms (100.00%) | Memory: 15.3 MB (33.81%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -6,17 +6,18 @@ public:
         
         while (left <= right) {
             int mid = (left + right)/2;
+
             if (nums[mid] == target)
                 return mid;
 
-            if (nums[mid] >= nums[left]) {
-                if (target >= nums[left] && target<nums[mid]) {
+            else if (nums[mid] >= nums[left]) {
+                if (target >= nums[left] && target < nums[mid]) {
                     right = mid-1;
                 } else {
                     left = mid+1;
                 }
             } else {
-                if (target <= nums[right] && target > nums[mid]) {
+                if (target > nums[mid] && target <= nums[right]) {
                     left = mid+1;
                 } else {
                     right = mid-1;
