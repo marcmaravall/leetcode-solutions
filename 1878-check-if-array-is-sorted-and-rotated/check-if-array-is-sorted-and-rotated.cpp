5e7a48d948da38297d64fde408b68412d031ea5c
Marc Maravall Díez <marcmaravall@gmail.com>
2026-05-23 20:52:44 +0200

    Time: 0 ms (100.00%) | Memory: 11.3 MB (53.59%) - LeetSync

--- 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.cpp
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.cpp
@@ -5,9 +5,8 @@ public:
         int n = nums.size();
 
         for (int i = 0; i < n; i++) {
-            if (nums[i] > nums[(i + 1) % n]) {
+            if (nums[i] > nums[(i + 1) % n])
                 count++;
-            }
         }
 
         return count <= 1;
