e883ea019ff90427f90e23eb5bfec7a172c209da
Marc Maravall Díez <marcmaravall@gmail.com>
2026-05-30 19:06:52 +0200

    Time: 8 ms (87.91%) | Memory: 16.1 MB (91.84%) - LeetSync

--- 4313-password-strength/password-strength.cpp
+++ 4313-password-strength/password-strength.cpp
@@ -8,22 +8,22 @@ public:
         }
 
         for (char c = 'a'; c <= 'z'; c++) {
-            if (freq[c] > 0) 
+            if (freq[c]) 
                 res++;
         }
         for (char c = 'A'; c <= 'Z'; c++) {
-            if (freq[c] > 0) 
+            if (freq[c]) 
                 res+=2;
         }
         for (char c = '0'; c <= '9'; c++) {
-            if (freq[c] > 0) 
+            if (freq[c]) 
                 res+=3;
         }
         
-        if (freq['!'] > 0) res += 5;
-        if (freq['@'] > 0) res += 5;
-        if (freq['#'] > 0) res += 5;
-        if (freq['$'] > 0) res += 5;
+        if (freq['!']) res += 5;
+        if (freq['@']) res += 5;
+        if (freq['#']) res += 5;
+        if (freq['$']) res += 5;
 
         return res;
     }
