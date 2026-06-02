b310215dd441c8634d1b5637b892cc536e387dba
Marc Maravall Díez <marcmaravall@gmail.com>
2026-06-02 22:32:35 +0200

    Time: 0 ms (100.00%) | Memory: 7.8 MB (71.29%) - LeetSync

---
+++ 357-count-numbers-with-unique-digits/count-numbers-with-unique-digits.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int countNumbersWithUniqueDigits(int n) {
+        std::vector<int> res = {
+            1, 
+            10, 
+            91, 
+            739, 
+            5275, 
+            32491, 
+            168571, 
+            712891, 
+            2345851
+        }; 
+
+        return res[n];
+    }
+};
