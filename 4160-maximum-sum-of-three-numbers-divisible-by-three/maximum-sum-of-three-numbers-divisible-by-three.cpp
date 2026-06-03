4549585ddcee264f6cf344a1cd4ef1fe924442de
Marc Maravall Díez <marcmaravall@gmail.com>
2026-06-03 22:13:35 +0200

    Time: 76 ms (79.42%) | Memory: 289.8 MB (59.81%) - LeetSync

---
+++ 4160-maximum-sum-of-three-numbers-divisible-by-three/maximum-sum-of-three-numbers-divisible-by-three.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int maximumSum(vector<int>& nums) {
+        std::vector<int> _0;
+        std::vector<int> _1;
+        std::vector<int> _2;
+
+        for (int n : nums) {
+            int mod = n % 3;
+            if (mod == 0) _0.push_back(n);
+            if (mod == 1) _1.push_back(n);
+            if (mod == 2) _2.push_back(n);
+        }
+        std::sort(_0.begin(), _0.end(), std::greater<int>());
+        std::sort(_1.begin(), _1.end(), std::greater<int>());
+        std::sort(_2.begin(), _2.end(), std::greater<int>());
+
+        int res = 0;
+        if (_0.size() >= 3)
+            res = max(res, _0[0] + _0[1] + _0[2]);
+        if (_1.size() >= 3)
+            res = max(res, _1[0] + _1[1] + _1[2]);
+        if (_2.size() >= 3)
+            res = max(res, _2[0] + _2[1] + _2[2]);
+        if (_0.size() >= 1 && _1.size() >= 1 && _2.size() >= 1)
+            res = max(res, _0[0] + _1[0] + _2[0]);
+    
+        return res;
+    }
+};
