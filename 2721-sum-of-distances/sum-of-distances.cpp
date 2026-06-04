d9b73029239eb3cef30a91f892a8fab9976f9744
Marc Maravall Díez <marcmaravall@gmail.com>
2026-06-04 20:19:36 +0200

    Time: 52 ms (67.81%) | Memory: 109.6 MB (79.59%) - LeetSync

---
+++ 2721-sum-of-distances/sum-of-distances.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    vector<long long> distance(vector<int>& nums) {
+        const int n = nums.size();
+        std::vector<long long> res(n);
+        std::unordered_map<int, std::vector<int>> mp;
+
+        for(int i = 0; i < n; i++)
+            mp[nums[i]].push_back(i);
+
+        for(auto &it : mp){
+            auto &pos = it.second;
+
+            long long sum = 0;
+            for(int x : pos) 
+                sum += x;
+
+            long long leftSum = 0;
+            int m = pos.size();
+
+            for(int i = 0; i < m; i++){
+                long long rightSum = sum - leftSum - pos[i];
+
+                long long left  = 1LL * pos[i] * i - leftSum;
+                long long right = rightSum - 1LL * pos[i] * (m-i-1);
+
+                res[pos[i]] = left + right;
+
+                leftSum += pos[i];
+            }
+        }
+
+        return res;
+    }
+};
