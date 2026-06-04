5d92aac8639ccbbd3e9cbd54fef2caef5f68cfbd
Marc Maravall Díez <marcmaravall@gmail.com>
2026-06-04 19:53:15 +0200

    Time: 43 ms (76.87%) | Memory: 9.4 MB (59.70%) - LeetSync

--- 4057-total-waviness-of-numbers-in-range-i/total-waviness-of-numbers-in-range-i.cpp
+++ 4057-total-waviness-of-numbers-in-range-i/total-waviness-of-numbers-in-range-i.cpp
@@ -1,31 +1,17 @@
 class Solution {
 public:
-    int waviness(int n) {
-        if (n <= 100)
-            return 0;
-
-        std::vector<int> digits;
-        while (n > 0) {
-            digits.push_back(n % 10);
-            n /= 10;
-        }
-        
-        int out = 0;
-        for (int i = 1; i < digits.size()-1; i++) {
-            if ((digits[i] > digits[i-1] && digits[i] > digits[i+1]) ||
-                (digits[i] < digits[i-1] && digits[i] < digits[i+1]))
-                out++;
-        }
-
-        return out;
-    }
-
     int totalWaviness(int num1, int num2) {
         int res = 0;
-        for (int i = num1; i <= num2; i++) {
-            res += waviness(i);
-        }
 
+        for (int i = num1; i <= num2; i++) {
+            std::string digits = std::to_string(i);
+            for (int j = 1; j < digits.size()-1; j++) {
+                if (digits[j] < digits[j+1] && digits[j] < digits[j-1] || 
+                    digits[j] > digits[j+1] && digits[j] > digits[j-1])
+                    res++;
+            }
+        }
+        
         return res;
     }
 };
