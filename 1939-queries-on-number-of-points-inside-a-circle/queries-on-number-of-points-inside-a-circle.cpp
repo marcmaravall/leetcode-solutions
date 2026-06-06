92c2f3899fc492f987dca3873067f18d52a62a4f
Marc Maravall Díez <marcmaravall@gmail.com>
2026-06-06 20:15:19 +0200

    Time: 47 ms (24.78%) | Memory: 20.3 MB (69.88%) - LeetSync

---
+++ 1939-queries-on-number-of-points-inside-a-circle/queries-on-number-of-points-inside-a-circle.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    inline bool inside(const std::vector<int>& query, const std::vector<int>& point) {
+        return sqrt(pow(point[0] - query[0], 2) + pow(point[1] - query[1], 2)) <= query[2]; 
+    }
+
+    vector<int> countPoints(vector<vector<int>>& points, vector<vector<int>>& queries) {
+        std::vector<int> res;
+
+        for (int i = 0; i < queries.size(); i++) {
+            int current = 0;
+            for (int j = 0; j < points.size(); j++) {
+                if (inside(queries[i], points[j])) {
+                    current++;
+                }
+            }
+            res.push_back(current);
+        }
+
+        return res;
+    }
+};
