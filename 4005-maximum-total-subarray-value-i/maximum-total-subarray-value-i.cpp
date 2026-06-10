5b375a7274682739a2b5efbb4033113daf7b7432
Marc Maravall Díez <marcmaravall@gmail.com>
2026-06-10 10:21:59 +0200

    Time: 0 ms (100.00%) | Memory: 103.8 MB (2.71%) - LeetSync

--- 4005-maximum-total-subarray-value-i/maximum-total-subarray-value-i.cpp
+++ 4005-maximum-total-subarray-value-i/maximum-total-subarray-value-i.cpp
@@ -2,10 +2,12 @@ class Solution {
 public:
     long long maxTotalValue(vector<int>& nums, int k) {
         int m = INT_MAX, M = 0;
+
         for (int n : nums) {
             m = min(n, m);
             M = max(n, M);
         }
+        
         return (long long)(M-m) * (long long)k;
     }
 };
