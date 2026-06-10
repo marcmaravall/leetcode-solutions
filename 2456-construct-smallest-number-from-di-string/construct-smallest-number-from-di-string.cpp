cb46c089de58f5311d3955828614bd5e1c684cad
Marc Maravall Díez <marcmaravall@gmail.com>
2026-06-10 11:05:41 +0200

    Time: 0 ms (100.00%) | Memory: 7.6 MB (96.03%) - LeetSync

---
+++ 2456-construct-smallest-number-from-di-string/construct-smallest-number-from-di-string.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string smallestNumber(string s) {
+        std::string res = "";
+        std::string stack = "";
+        
+        for (int i = 0; i <= s.length(); i++) {
+            stack.push_back('1' + i);
+            if (i == s.length() || s[i] == 'I') {
+                while (!stack.empty()) {
+                    res.push_back(stack.back());
+                    stack.pop_back();
+                }
+            }
+        }
+
+        return res;
+    }
+};
