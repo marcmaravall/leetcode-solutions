ab90eb825496748c90535ac50317a624c64ea9a2
Marc Maravall Díez <marcmaravall@gmail.com>
2026-06-11 16:24:42 +0200

    Time: 280 ms (69.13%) | Memory: 331.2 MB (89.26%) - LeetSync

---
+++ 3844-number-of-ways-to-assign-edge-weights-i/number-of-ways-to-assign-edge-weights-i.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    long long pow(long long base, int exp) {
+        constexpr int mod = 1000000007;
+        long long res = 1;
+
+        while (exp) {
+            if (exp % 2 == 1)
+                res = res * base % mod;
+
+            base = base * base % mod;
+            exp /= 2;
+        }
+
+        return res;
+    }
+
+    int assignEdgeWeights(vector<vector<int>>& edges) {
+        const int n = edges.size();
+        std::vector<std::vector<int>> graph(edges.size()+2);
+
+        for (auto& edge : edges) {
+            graph[edge[0]].push_back(edge[1]);
+            graph[edge[1]].push_back(edge[0]);
+        }
+
+        int maxDepth = 0;
+        auto dfs = [&](this auto&& dfs, int node, int prev) -> int {
+            int res = 0;
+
+            for (auto& x : graph[node])
+                if (x != prev)
+                    res = max(res, dfs(x, node) + 1);
+
+            return res;
+        };
+        maxDepth = dfs(1, 0);
+
+        return pow(2, maxDepth-1);
+    }
+};
