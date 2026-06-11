8c9b9a95b1874aff8ef060b3b6c3a3b29d89da9e
Marc Maravall Díez <marcmaravall@gmail.com>
2026-06-11 17:02:38 +0200

    Time: 0 ms (100.00%) | Memory: 14.9 MB (52.02%) - LeetSync

---
+++ 813-all-paths-from-source-to-target/all-paths-from-source-to-target.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
+        std::vector<std::vector<int>> res;
+
+        const int target = graph.size()-1;
+        std::function<void(std::vector<int>&, int)> dfs = [&](std::vector<int>& vec, int current) -> void {
+            if (current == target) {
+                res.push_back(vec);
+                return;
+            }
+
+            for (int node : graph[current]) {
+                vec.push_back(node);
+                dfs(vec, node);
+                vec.pop_back();
+            }
+        };
+
+        std::vector<int> vec { 0 };
+        dfs(vec, 0);
+
+        return res;
+    }
+};
