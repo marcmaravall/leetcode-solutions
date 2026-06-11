c71f94b4cff3cd32d609dcfa4b504361e5a458f3
Marc Maravall Díez <marcmaravall@gmail.com>
2026-06-11 17:24:04 +0200

    Time: 7 ms (48.95%) | Memory: 98.1 MB (95.32%) - LeetSync

--- 1661-minimum-number-of-vertices-to-reach-all-nodes/minimum-number-of-vertices-to-reach-all-nodes.cpp
+++ 1661-minimum-number-of-vertices-to-reach-all-nodes/minimum-number-of-vertices-to-reach-all-nodes.cpp
@@ -1,22 +1,17 @@
 class Solution {
 public:
     vector<int> findSmallestSetOfVertices(int n, vector<vector<int>>& edges) {
-        std::vector<std::vector<int>> graph(n);
+        std::vector<bool> v(n, 0);
         for (auto& e : edges) {
-            graph[e[1]].push_back(e[0]);
+            v[e[1]] = true;
         }
 
-        std::unordered_set<int> uset;
-        std::function<int(int)> parent = [&](int node) {
-            if (graph[node].size() == 0)
-                return node;
-            return parent(graph[node][0]);
-        };
-
-        for (auto& e : edges) {
-            uset.emplace(parent(e[0]));
+        std::vector<int> res;
+        for (int i = 0; i < n; i++) {
+            if (!v[i])
+                res.push_back(i);
         }
-        std::vector<int> res(uset.begin(), uset.end());
+
         return res;
     }
 };
