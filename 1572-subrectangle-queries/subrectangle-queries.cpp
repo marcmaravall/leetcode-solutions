e1ffc819296e22c7f36a73b88dd4b5b164a7ce9e
Marc Maravall Díez <marcmaravall@gmail.com>
2026-06-12 18:28:26 +0200

    Time: 32 ms (26.37%) | Memory: 25.1 MB (93.19%) - LeetSync

---
+++ 1572-subrectangle-queries/subrectangle-queries.cpp
@@ -0,0 +1,28 @@
+class SubrectangleQueries {
+private:
+    std::vector<std::vector<int>> rectangle;
+
+public:
+    SubrectangleQueries(vector<vector<int>>& rectangle) {
+        this->rectangle = std::move(rectangle);
+    }
+    
+    void updateSubrectangle(int row1, int col1, int row2, int col2, int newValue) {
+        for (int r = row1; r <= row2; r++) {
+            for (int c = col1; c <= col2; c++) {
+                rectangle[r][c] = newValue;
+            }
+        }
+    }
+    
+    int getValue(int row, int col) {
+        return rectangle[row][col];
+    }
+};
+
+/**
+ * Your SubrectangleQueries object will be instantiated and called as such:
+ * SubrectangleQueries* obj = new SubrectangleQueries(rectangle);
+ * obj->updateSubrectangle(row1,col1,row2,col2,newValue);
+ * int param_2 = obj->getValue(row,col);
+ */
