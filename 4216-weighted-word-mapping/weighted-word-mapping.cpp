7920fd3128f44139397bd44fe495ad6fcd5170ee
Marc Maravall Díez <marcmaravall@gmail.com>
2026-06-13 18:36:23 +0200

    Time: 0 ms (100.00%) | Memory: 43.6 MB (77.60%) - LeetSync

---
+++ 4216-weighted-word-mapping/weighted-word-mapping.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    string mapWordWeights(vector<string>& words, vector<int>& weights) {
+        std::string res = "";
+        for (std::string& word : words) {
+            int current = 0;
+            for (char c : word) {
+                current += weights[c - 'a'];
+            }
+            current %= 26;
+            res += 'z' - (char)current;
+        }
+        return res;
+    }
+};
