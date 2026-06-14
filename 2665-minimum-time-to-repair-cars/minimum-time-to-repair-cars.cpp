2f467c782be6c0f46a58f5f4acab55238a592f65
Marc Maravall Díez <marcmaravall@gmail.com>
2026-06-14 22:24:44 +0200

    Time: 23 ms (79.80%) | Memory: 59.4 MB (48.93%) - LeetSync

--- 2665-minimum-time-to-repair-cars/minimum-time-to-repair-cars.cpp
+++ 2665-minimum-time-to-repair-cars/minimum-time-to-repair-cars.cpp
@@ -1,27 +1,29 @@
-// this is hard i couldnt solve it
 class Solution {
 public:
     long long repairCars(vector<int>& ranks, int cars) {
-        long long left = 1, right = (long long)*min_element(ranks.begin(), ranks.end()) * cars * cars;
-        
-        auto can_repair_all = [&](long long time) {
-            long long total_cars_repaired = 0;
-            for (int rank : ranks) {
-                total_cars_repaired += sqrt(time / rank);
-                if (total_cars_repaired >= cars) return true;
+        long long left = 1;
+        long long right = (long long)*std::min_element(ranks.begin(), ranks.end()) * cars*cars;
+
+        auto canRepair = [&](long long time) -> bool {
+            int repaired = 0;
+            for (int r : ranks) {
+                repaired += sqrt(time/r);
+                if (repaired >= cars) {
+                    return true;
+                }
             }
+
             return false;
         };
-        
+
         while (left < right) {
             long long mid = (left + right) / 2;
-            if (can_repair_all(mid)) {
+            if (canRepair(mid)) {
                 right = mid;
-            } else {
-                left = mid + 1;
-            }
+            } else
+                left = mid+1;
         }
-        
-        return left;
+
+        return right;
     }
 };
