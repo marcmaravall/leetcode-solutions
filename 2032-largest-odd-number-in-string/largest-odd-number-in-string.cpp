77455a904e70fa476a856b557d4453c2b592f3d1
Marc Maravall Díez <marcmaravall@gmail.com>
2026-06-15 20:16:24 +0200

    Time: 0 ms (100.00%) | Memory: 17.9 MB (17.79%) - LeetSync

--- 2032-largest-odd-number-in-string/largest-odd-number-in-string.cpp
+++ 2032-largest-odd-number-in-string/largest-odd-number-in-string.cpp
@@ -1,17 +1,10 @@
 class Solution {
 public:
     string largestOddNumber(string num) {
-        const int n = num.size();
-        for (int i = n-1; i >= 0; i--) {
-            if (num[i] == '1' || num[i] == '3' || num[i] == '5' || num[i] == '7' || num[i] == '9') {
-                std::string res = "";
-                for (int j = 0; j <= i; j++) {
-                    res += num[j];
-                }
-                return res;
-            }
+        for(int i = num.length()-1; i>=0; i--){
+            if (num[i]%2) 
+                return num.substr(0,i+1);
         }
-
         return "";
     }
 };
