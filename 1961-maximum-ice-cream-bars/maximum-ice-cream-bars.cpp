2e1468e7526b8b624a10ba78c1677d12e8e522ac
Marc Maravall Díez <marcmaravall@gmail.com>
2026-06-21 20:18:53 +0200

    Time: 39 ms (30.88%) | Memory: 80.3 MB (37.51%) - LeetSync

--- 1961-maximum-ice-cream-bars/maximum-ice-cream-bars.cpp
+++ 1961-maximum-ice-cream-bars/maximum-ice-cream-bars.cpp
@@ -2,8 +2,8 @@ class Solution {
 public:
     int maxIceCream(vector<int>& costs, int coins) {
         std::sort(costs.begin(), costs.end());
-        int res = 0;
 
+        int res = 0;
         for (int c : costs) {
             coins -= c;
             if (coins < 0)
