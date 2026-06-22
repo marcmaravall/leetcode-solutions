c6b939a0b0687b1cb0b3e8c309970edf4b82fd6e
Marc Maravall Díez <marcmaravall@gmail.com>
2026-06-22 20:42:09 +0200

    Time: 2 ms (15.95%) | Memory: 8.5 MB (60.54%) - LeetSync

---
+++ 2372-rearrange-characters-to-make-target-string/rearrange-characters-to-make-target-string.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int rearrangeCharacters(string s, string target) {
+        std::vector<int> freqs(52);
+        for (char c : s) freqs[c-'a']++;
+        for (char c : target) freqs[c-'a'+26]++;
+
+        int res = INT_MAX;
+        for (char c : target) {
+            res = min(res, freqs[c-'a'] / freqs[c-'a'+26]);
+        }
+        return res;
+    }
+};
