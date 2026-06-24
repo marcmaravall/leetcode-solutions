693a7725f5f98cff49b9bbf27822cc1cc70211ba
Marc Maravall Díez <marcmaravall@gmail.com>
2026-06-24 13:27:55 +0200

    Time: 6 ms (60.81%) | Memory: 20.8 MB (75.79%) - LeetSync

---
+++ 2447-merge-similar-items/merge-similar-items.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<vector<int>> mergeSimilarItems(vector<vector<int>>& items1, vector<vector<int>>& items2) {
+        std::vector<std::vector<int>> res;
+        std::unordered_map<int, int> weights;
+        for (auto& item : items1) weights[item[0]] += item[1];
+        for (auto& item : items2) weights[item[0]] += item[1];
+        
+        for (auto& pair : weights) {
+            res.push_back({pair.first, pair.second});
+        }
+
+        std::sort(res.begin(), res.end());
+        return res;
+    }
+};
