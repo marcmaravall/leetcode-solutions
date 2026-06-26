cc999cd1573c4159666d3facb8bd204b2924a77c
Marc Maravall Díez <marcmaravall@gmail.com>
2026-06-26 12:44:07 +0200

    Time: 8 ms (89.40%) | Memory: 101.6 MB (63.58%) - LeetSync

---
+++ 4075-count-subarrays-with-majority-element-ii/count-subarrays-with-majority-element-ii.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    long long countMajoritySubarrays(vector<int>& nums, int target) {
+        int n = nums.size();
+        long long res = 0;
+        int pre = n + 1;
+        
+        std::vector<int> count(2*n+2), acc(2*n+2);
+        count[pre] = 1, acc[pre] = 1;
+
+        for (int x : nums) {
+            pre += (x == target ? 1 : -1);
+            count[pre]++;
+            acc[pre] = acc[pre - 1] + count[pre];
+            res += acc[pre - 1];
+        }
+        
+        return res;
+    }
+};
