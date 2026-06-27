30ac3cbeb6d0bcd1a571b75d31d2027386d5d501
Marc Maravall Díez <marcmaravall@gmail.com>
2026-06-27 13:12:28 +0200

    Time: 20 ms (85.99%) | Memory: 21.2 MB (44.28%) - LeetSync

---
+++ 690-employee-importance/employee-importance.cpp
@@ -0,0 +1,28 @@
+/*
+// Definition for Employee.
+class Employee {
+public:
+    int id;
+    int importance;
+    vector<int> subordinates;
+};
+*/
+
+class Solution {
+public:
+    int getImportance(vector<Employee*> employees, int id) {
+        std::unordered_map<int, Employee*> graph;
+        for (auto* e : employees)
+            graph[e->id] = e;
+        
+        std::function<int(int)> importance = [&](int id) -> int {
+            int res = graph[id]->importance;
+            for (int e : graph[id]->subordinates) {
+                res += importance(e);
+            }
+            return res;
+        };
+
+        return importance(id);
+    }
+};
