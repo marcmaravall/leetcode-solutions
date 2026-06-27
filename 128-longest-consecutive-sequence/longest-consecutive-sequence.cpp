5fd951aee91649d2fb65729851ed94d94b4ff103
Marc Maravall Díez <marcmaravall@gmail.com>
2026-06-27 13:18:45 +0200

    Time: 15 ms (89.80%) | Memory: 60.2 MB (92.67%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
-        if (nums.size() == 0)
+        const int n = nums.size();
+        if (n == 0)
             return 0;
-            
+
         std::sort(nums.begin(), nums.end());
         int res = 1;
-        const int n = nums.size();
         for (int i = 0; i < n-1; i++) {
             int len = 1;
             while (i < n-1 && nums[i]+1 == nums[i+1]) {
