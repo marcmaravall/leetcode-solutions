b4ac3a9f81027830460947238c20701511b425d7
Marc Maravall Díez <marcmaravall@gmail.com>
2026-06-28 16:45:46 +0200

    Time: 0 ms (100.00%) | Memory: 8.4 MB (17.09%) - LeetSync

--- 1954-replace-all-digits-with-characters/replace-all-digits-with-characters.cpp
+++ 1954-replace-all-digits-with-characters/replace-all-digits-with-characters.cpp
@@ -2,9 +2,8 @@ class Solution {
 public:
     string replaceDigits(string s) {
         const int n = s.size();
-        for (int i = 1; i < n; i+=2) {
+        for (int i = 1; i < n; i+=2)
             s[i] = s[i-1] + (s[i]-'0');
-        }
         return s;
     }
 };
