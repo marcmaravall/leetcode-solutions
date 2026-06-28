e2de41691d0f6e747582395946dfd44808b7cae5
Marc Maravall Díez <marcmaravall@gmail.com>
2026-06-28 16:08:12 +0200

    Time: 15 ms (12.43%) | Memory: 55.1 MB (69.21%) - LeetSync

--- 1956-maximum-element-after-decreasing-and-rearranging/maximum-element-after-decreasing-and-rearranging.cpp
+++ 1956-maximum-element-after-decreasing-and-rearranging/maximum-element-after-decreasing-and-rearranging.cpp
@@ -1,14 +1,15 @@
 class Solution {
 public:
     int maximumElementAfterDecrementingAndRearranging(vector<int>& arr) {
-        int res = 1;
         std::sort(arr.begin(), arr.end());
-        const int n = arr.size();
         arr[0] = 1;
+        
+        int res = 1;
+        const int n = arr.size();
         for (int i = 1; i < n; i++) {
-            if (arr[i]-arr[i-1] > 1) {
+            if (arr[i]-arr[i-1] > 1) 
                 arr[i] = arr[i-1]+1;
-            }
+
             res = max(res, arr[i]);
         }
 
