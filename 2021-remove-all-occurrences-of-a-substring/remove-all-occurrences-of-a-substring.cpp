ef9b0a2922a3b603db4b8b4f95b46a786eefca2e
Marc Maravall Díez <marcmaravall@gmail.com>
2026-06-29 09:37:05 +0200

    Time: 0 ms (100.00%) | Memory: 9.3 MB (93.30%) - LeetSync

--- 2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.cpp
+++ 2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.cpp
@@ -1,10 +1,8 @@
 class Solution {
 public:
     string removeOccurrences(string s, string part) {
-        size_t start = s.find(part);
-        if (start == std::string::npos)
-            return s;
-        s.erase(start, part.size());
-        return removeOccurrences(s, part);
+        for (size_t start = s.find(part); start != std::string::npos; start = s.find(part))
+            s.erase(start, part.size());
+        return s;
     }
 };
