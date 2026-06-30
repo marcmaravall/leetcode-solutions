5623652f2b7229edcf7f45bbc8a880287ae12b7f
Marc Maravall Díez <marcmaravall@gmail.com>
2026-06-30 14:22:59 +0200

    Time: 0 ms (100.00%) | Memory: 69.6 MB (27.18%) - LeetSync

---
+++ 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int longestOnes(vector<int>& nums, int k) {
+        int i = 0, j = 0;
+        const int n = nums.size();
+        for (i = 0; i < n; i++) {
+            if (nums[i] == 0) 
+                k--;
+            if (k < 0 && nums[j++] == 0) 
+                k++;
+        }
+        return i-j;
+    }
+};
