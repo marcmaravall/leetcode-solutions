70738cad0f2bee8460f03fae794dcf9ee2ec23d6
Marc Maravall Díez <marcmaravall@gmail.com>
2026-07-02 17:04:50 +0200

    Time: 15 ms (47.58%) | Memory: 11.2 MB (54.61%) - LeetSync

---
+++ 1263-number-of-dice-rolls-with-target-sum/number-of-dice-rolls-with-target-sum.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+#define MOD 1000000007
+    int numRollsToTarget(int d, int f, int target) {
+        std::vector<int> dp(target + 1);
+        dp[0] = 1;
+        
+        for (int i = 1; i <= d; ++i) {
+            std::vector<int> dp1(target + 1);
+            for (int j = 1; j <= f; ++j)
+                for (auto k = j; k <= target; ++k)
+                    dp1[k] = (dp1[k] + dp[k - j]) % 1000000007;
+            swap(dp, dp1);
+        }
+        
+        return dp[target];
+    }
+};
