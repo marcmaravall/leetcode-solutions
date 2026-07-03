e1a1ea67175d5446e0b38bb202cfc7c64289ec1f
Marc Maravall Díez <marcmaravall@gmail.com>
2026-07-03 15:53:33 +0200

    Time: 44 ms (41.34%) | Memory: 90 MB (6.93%) - LeetSync

---
+++ 1620-check-if-array-pairs-are-divisible-by-k/check-if-array-pairs-are-divisible-by-k.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool canArrange(vector<int>& arr, int k) {
+        std::unordered_map<int, int> freq;
+        for (int n : arr) {
+            int r = ((n % k) + k) % k;
+            freq[r]++;
+        }
+
+        if (freq[0] % 2 != 0)
+            return false;
+
+        for (int i = 1; i < (k + 1) / 2; i++) {
+            if (freq[i] != freq[k - i])
+                return false;
+        }
+
+        if (k % 2 == 0 && freq[k / 2] % 2 != 0)
+            return false;
+
+        return true;
+    }
+};
