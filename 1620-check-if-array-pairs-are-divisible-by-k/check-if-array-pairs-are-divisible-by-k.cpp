da89d46112897f1d4a4446709e9c48d2c41d962e
Marc Maravall Díez <marcmaravall@gmail.com>
2026-07-03 15:54:28 +0200

    Time: 59 ms (16.65%) | Memory: 89.7 MB (12.74%) - LeetSync

--- 1620-check-if-array-pairs-are-divisible-by-k/check-if-array-pairs-are-divisible-by-k.cpp
+++ 1620-check-if-array-pairs-are-divisible-by-k/check-if-array-pairs-are-divisible-by-k.cpp
@@ -9,14 +9,13 @@ public:
 
         if (freq[0] % 2 != 0)
             return false;
+        if (k % 2 == 0 && freq[k / 2] % 2 != 0)
+            return false;
 
         for (int i = 1; i < (k + 1) / 2; i++) {
             if (freq[i] != freq[k - i])
                 return false;
         }
-
-        if (k % 2 == 0 && freq[k / 2] % 2 != 0)
-            return false;
 
         return true;
     }
