078478c6353818317da59af9efb22c328b8996cc
Marc Maravall Díez <marcmaravall@gmail.com>
2026-07-03 15:08:00 +0200

    Time: 53 ms (92.46%) | Memory: 80.2 MB (81.41%) - LeetSync

--- 2548-destroy-sequential-targets/destroy-sequential-targets.cpp
+++ 2548-destroy-sequential-targets/destroy-sequential-targets.cpp
@@ -1,17 +1,14 @@
 class Solution {
 public:
     int destroyTargets(vector<int>& nums, int space) {
-        int res = INT_MAX;
         int m = 0;
         std::unordered_map<int, int> freq;
-        
         for (int n : nums)
-            m = std::max(m, ++freq[n % space]);
-    
-        for (int n : nums) {
+            m = max(m, ++freq[n%space]);
+        int res = INT_MAX;
+        for (int n : nums)
             if (freq[n % space] == m)
-                res = min(res, n);
-        }
+                res = std::min(res, n);
         return res;
     }
 };
