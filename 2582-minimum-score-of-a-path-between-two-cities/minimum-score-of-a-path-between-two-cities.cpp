bec057e39184690da5ea471a9c628bc127e1e961
Marc Maravall Díez <marcmaravall@gmail.com>
2026-07-04 20:27:21 +0200

    Time: 14 ms (92.92%) | Memory: 106.6 MB (98.62%) - LeetSync

---
+++ 2582-minimum-score-of-a-path-between-two-cities/minimum-score-of-a-path-between-two-cities.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int minScore(int n, vector<vector<int>>& roads) {
+        std::vector<int> root(n+1);
+        std::iota(root.begin(), root.end(), 0);
+
+        auto find = [&](this auto& self, int i) -> int {
+            return root[i] == i ? i : root[i] = self(root[i]);  
+        };
+
+        for (auto& r : roads)
+            root[find(r[0])] = find(r[1]);
+        
+        int res = INT_MAX;
+        for (auto& r : roads) {
+            if (find(r[0]) == find(1))
+                res = min(res, r[2]);
+        }
+        return res;
+    }
+};
