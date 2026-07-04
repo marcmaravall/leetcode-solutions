fe6182edd808f3ba9b9751e2f793b7d4c5e273ec
Marc Maravall Díez <marcmaravall@gmail.com>
2026-07-04 20:35:07 +0200

    Time: 1883 ms (5.01%) | Memory: 106 MB (99.83%) - LeetSync

--- 2582-minimum-score-of-a-path-between-two-cities/minimum-score-of-a-path-between-two-cities.cpp
+++ 2582-minimum-score-of-a-path-between-two-cities/minimum-score-of-a-path-between-two-cities.cpp
@@ -5,17 +5,16 @@ public:
         std::iota(root.begin(), root.end(), 0);
 
         auto find = [&](this auto& self, int i) -> int {
-            return root[i] == i ? i : root[i] = self(root[i]);  
+            return root[i] == i ? i : self(root[i]);
         };
 
         for (auto& r : roads)
             root[find(r[0])] = find(r[1]);
         
         int res = INT_MAX;
-        for (auto& r : roads) {
+        for (auto& r : roads)
             if (find(r[0]) == find(1))
                 res = min(res, r[2]);
-        }
         return res;
     }
 };
