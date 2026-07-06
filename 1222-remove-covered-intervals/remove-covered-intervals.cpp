c80d32828dcb50318ddd3a1ff37fc81c747c2760
Marc Maravall Díez <marcmaravall@gmail.com>
2026-07-06 20:42:11 +0200

    Time: 0 ms (100.00%) | Memory: 15.1 MB (60.13%) - LeetSync

--- 1222-remove-covered-intervals/remove-covered-intervals.cpp
+++ 1222-remove-covered-intervals/remove-covered-intervals.cpp
@@ -2,14 +2,14 @@ class Solution {
 public:
     int removeCoveredIntervals(vector<vector<int>>& intervals) {
         std::ranges::sort(intervals, {}, [](auto& x) {
-            return pair{x[0], -x[1]};
+            return std::pair(x[0], -x[1]);
         });
-        
+
         int res = 0, r = 0;
-        for (auto& interval : intervals) {
-            if (interval[1] > r)
+        for (auto& x : intervals) {
+            if (x[1] > r)
                 res++;
-            r = std::max(r, interval[1]);
+            r = std::max(r, x[1]);
         }
         return res;
     }
