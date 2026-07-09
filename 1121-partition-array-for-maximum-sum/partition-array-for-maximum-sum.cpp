244e3bbe19278815fa7bb805ce5e4798a021e97f
Marc Maravall Díez <marcmaravall@gmail.com>
2026-07-09 18:37:19 +0200

    Time: 0 ms (100.00%) | Memory: 12.3 MB (88.64%) - LeetSync

---
+++ 1121-partition-array-for-maximum-sum/partition-array-for-maximum-sum.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maxSumAfterPartitioning(vector<int>& arr, int k) {
+        const int n = arr.size();
+        std::vector<int> dp(n+1);
+        for (int i = 0; i <= n; i++) {
+            int m = 0, best = 0;
+            for (int j = 1; j <= k && i-j >= 0; j++) {
+                m = std::max(m, arr[i-j]);
+                best = std::max(best, dp[i-j] + m*j);
+            }
+
+            dp[i] = best;
+        }
+        return dp[n];
+    }
+};
