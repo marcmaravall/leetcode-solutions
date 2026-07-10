53ec2ec27f3b56404b22942d1010f5149dde2f36
Marc Maravall Díez <marcmaravall@gmail.com>
2026-07-10 20:34:16 +0200

    Time: 2 ms (87.20%) | Memory: 7.8 MB (88.15%) - LeetSync

---
+++ 2397-count-number-of-ways-to-place-houses/count-number-of-ways-to-place-houses.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int countHousePlacements(int n) {
+        constexpr long long mod = 1e9+7;
+        long long house = 1, space = 1;
+        long long total = house+space;
+        for (int i = 2; i <= n; i++) {
+            house = space;
+            space = total;
+            total = (space + house) % mod;
+        }
+        return (total*total)%mod;
+    }
+};
