dfd584f412dd4008581abbef353ec19d068d44b4
Marc Maravall Díez <marcmaravall@gmail.com>
2026-07-15 23:33:37 +0200

    Time: 27 ms (69.48%) | Memory: 57.2 MB (79.00%) - LeetSync

---
+++ 1683-maximum-number-of-coins-you-can-get/maximum-number-of-coins-you-can-get.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maxCoins(vector<int>& piles) {
+        std::sort(piles.begin(), piles.end(), std::greater<int>());
+        const int n = piles.size();
+        for (int i = 0; i < n; i+=2) {
+            piles[i] = 0;
+        }
+        for (int i = 0; i < n/3; i++) {
+            piles[n-i-1] = 0;
+        }
+        return std::accumulate(piles.begin(), piles.end(), 0);
+    }
+};
