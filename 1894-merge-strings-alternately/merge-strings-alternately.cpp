30aa59273ad06866f3c15084083e63475836efb4
Marc Maravall Díez <marcmaravall@gmail.com>
2026-07-16 22:45:36 +0200

    Time: 0 ms (100.00%) | Memory: 8.3 MB (84.18%) - LeetSync

---
+++ 1894-merge-strings-alternately/merge-strings-alternately.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    string mergeAlternately(string word1, string word2) {
+        std::string res = "";
+        const int n1 = word1.size();
+        const int n2 = word2.size();
+        for (int i = 0; i < std::max(n1, n2); i++) {
+            if (i < n1)
+                res += word1[i];
+            if (i < n2)
+                res += word2[i];
+        }
+        return res;
+    }
+};
