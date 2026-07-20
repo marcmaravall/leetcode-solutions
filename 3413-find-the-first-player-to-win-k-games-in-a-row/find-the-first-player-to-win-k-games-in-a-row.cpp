c69e566053377706dfd58c588af46dbb0f7cbf20
Marc Maravall Díez <marcmaravall@gmail.com>
2026-07-20 20:26:54 +0200

    Time: 6 ms (38.39%) | Memory: 120.7 MB (71.09%) - LeetSync

--- 3413-find-the-first-player-to-win-k-games-in-a-row/find-the-first-player-to-win-k-games-in-a-row.cpp
+++ 3413-find-the-first-player-to-win-k-games-in-a-row/find-the-first-player-to-win-k-games-in-a-row.cpp
@@ -2,25 +2,20 @@ class Solution {
 public:
     int findWinningPlayer(vector<int>& skills, int k) {
         const int n = skills.size();
-        std::deque<int> queue;
-        for (int i = 1; i < n; i++) 
-            queue.push_back(i);
         int winner = 0;
-        int winnerCount = 0;
-        while (1) {
-            int current = queue.front();
-            queue.pop_front();
-            if (skills[current] < skills[winner]) {
-                winnerCount++;
-                queue.push_back(current);
+        int wins = 0;
+        for (int i = 1; i < n; i++) {
+            if (skills[winner] > skills[i]) {
+                wins++;
             } else {
-                winnerCount = 1;
-                queue.push_back(winner);
-                winner = current;
+                winner = i;
+                wins = 1;
             }
-            if (winnerCount == k || winnerCount >= n-1)
+            
+            if (wins == k)
                 return winner;
         }
-        return -1;
+
+        return winner;
     }
 };
