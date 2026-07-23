4623053526953ffca84fb3155e83c0c661c1cce1
Marc Maravall Díez <marcmaravall@gmail.com>
2026-07-23 16:52:09 +0200

    Time: 4 ms (28.94%) | Memory: 22.9 MB (85.43%) - LeetSync

--- 1028-interval-list-intersections/interval-list-intersections.cpp
+++ 1028-interval-list-intersections/interval-list-intersections.cpp
@@ -6,15 +6,13 @@ public:
         const int m = secondList.size();
         std::vector<std::vector<int>> res;
         while (i < n && j < m) {
-            int aStart = firstList[i][0];
-            int aEnd = firstList[i][1];
-            int bStart = secondList[j][0];
-            int bEnd = secondList[j][1];
-            if (aStart <= bEnd && bStart <= aEnd) {
+            const int aStart = firstList[i][0];
+            const int aEnd = firstList[i][1];
+            const int bStart = secondList[j][0];
+            const int bEnd = secondList[j][1];
+            if (aStart <= bEnd && bStart <= aEnd)
                 res.push_back({std::max(aStart, bStart), std::min(aEnd, bEnd)});
-            }
-            if (aEnd <= bEnd)
-                i++;
+            if (aEnd <= bEnd) i++;
             else j++;
         }
         return res;
