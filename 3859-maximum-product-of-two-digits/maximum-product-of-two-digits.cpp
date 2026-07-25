cfe6326365a6625572661306f7e28e8602fce034
Marc Maravall Díez <marcmaravall@gmail.com>
2026-07-25 22:29:54 +0200

    Time: 0 ms (100.00%) | Memory: 8.7 MB (59.04%) - LeetSync

---
+++ 3859-maximum-product-of-two-digits/maximum-product-of-two-digits.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int maxProduct(int n) {
+
+        int m1 = 0;
+        int m2 = 0;
+        
+        while (n != 0) {
+            int d = (n % 10);
+            
+            if (d > m1) {
+                m2 = m1; 
+                m1 = d;    
+            } else if (d > m2) {
+                m2 = d;    
+            }
+            
+            n /= 10;
+        }
+
+        return m1*m2;
+    }
+};
