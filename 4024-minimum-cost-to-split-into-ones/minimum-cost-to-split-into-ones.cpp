0b9af2a2d783b76b98904fa8780c50ddcf229ac9
Marc Maravall Díez <marcmaravall@gmail.com>
2026-07-27 22:19:25 +0200

    Time: 0 ms (100.00%) | Memory: 8.2 MB (36.22%) - LeetSync

--- 4024-minimum-cost-to-split-into-ones/minimum-cost-to-split-into-ones.cpp
+++ 4024-minimum-cost-to-split-into-ones/minimum-cost-to-split-into-ones.cpp
@@ -2,9 +2,8 @@ class Solution {
 public:
     int minCost(int n) {
         int res = 0;
-        for (int i = n-1; i >= 1; i--) {
+        for (int i = n-1; i >= 1; i--)
             res+=i;
-        }
         return res;
     }
 };
