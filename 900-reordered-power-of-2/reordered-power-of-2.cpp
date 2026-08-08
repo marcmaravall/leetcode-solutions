98055762d23b5938cdfcb79ed71c2ed17832c945
Marc Maravall Díez <marcmaravall@gmail.com>
2026-08-08 23:22:48 +0200

    Time: 0 ms (100.00%) | Memory: 8.1 MB (76.38%) - LeetSync

---
+++ 900-reordered-power-of-2/reordered-power-of-2.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    bool reorderedPowerOf2(int n) {
+        std::vector<std::array<int, 10>> freq(31, std::array<int, 10>());
+        int pow = 1;
+        for (int i = 0; pow < (1 << 30); i++) {
+            int cpy = pow;
+            while (cpy > 0) {
+                int digit = cpy%10;
+                freq[i][digit]++;
+                cpy /= 10;
+            }
+            pow <<= 1;
+        }
+
+        std::array<int, 10> nfreq {};
+        while (n > 0) {
+            int digit = n % 10;
+            nfreq[digit]++;
+            n /= 10;
+        }
+
+        for (auto& f : freq) {
+            bool res = true;
+            for (int i = 0; i <= 9; i++) {
+                if (f[i] != nfreq[i]) {
+                    res = false;
+                    break;
+                }
+            }
+            if (res)
+                return true;
+        }
+
+        return false;
+    }
+};
