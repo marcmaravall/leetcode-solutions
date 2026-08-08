f4f60d36d77a3c5fd9576ec8cd043238eab9b4c8
Marc Maravall Díez <marcmaravall@gmail.com>
2026-08-08 23:08:09 +0200

    Time: 0 ms (100.00%) | Memory: 31.3 MB (37.89%) - LeetSync

--- 4354-unique-middle-element/unique-middle-element.cpp
+++ 4354-unique-middle-element/unique-middle-element.cpp
@@ -1,13 +1,14 @@
 class Solution {
 public:
     bool isMiddleElementUnique(vector<int>& nums) {
-        std::vector<int> freq(101, 0);
         const int n = nums.size();
+        int midFreq = 0;
         int mid = nums[n/2];
         for (int i = 0; i < n; i++) {
-            freq[nums[i]]++;
-            if (freq[mid] > 1)
-                return false;
+            if (nums[i] == mid) {
+                if (++midFreq > 1)
+                    return false;
+            }
         }
         return true;
     }
