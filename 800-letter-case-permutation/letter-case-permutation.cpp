25320b85b654b04ce76e981a4157fcd1750bc2ef
Marc Maravall Díez <marcmaravall@gmail.com>
2026-08-13 14:24:11 +0200

    Time: 1 ms (67.64%) | Memory: 13.1 MB (79.43%) - LeetSync

---
+++ 800-letter-case-permutation/letter-case-permutation.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    void backtrack(std::string& s, int i, std::vector<std::string>& vec) {
+        if (i >= s.size()) {
+            vec.push_back(s);
+            return;
+        }
+        char curr = s[i];
+        if (curr >= '0' && curr <= '9') {
+            backtrack(s, i+1, vec);
+            return;
+        }
+
+        s[i] = std::tolower(curr);
+        backtrack(s, i+1, vec);
+
+        s[i] = std::toupper(curr);
+        backtrack(s, i+1, vec);
+        
+        s[i] = curr;
+    }
+
+    vector<string> letterCasePermutation(string s) {
+        std::vector<std::string> res;
+        backtrack(s, 0, res);
+        return res;
+    }
+};
