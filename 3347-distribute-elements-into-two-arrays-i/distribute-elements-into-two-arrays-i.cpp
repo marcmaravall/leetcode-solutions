23618c4fe04157537508f1ac538b702e752d865e
Marc Maravall Díez <marcmaravall@gmail.com>
2026-08-20 23:18:23 +0200

    Time: 0 ms (100.00%) | Memory: 23.9 MB (46.12%) - LeetSync

---
+++ 3347-distribute-elements-into-two-arrays-i/distribute-elements-into-two-arrays-i.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> resultArray(vector<int>& nums) {
+        std::vector<int> arr1, arr2;
+        arr1.push_back(nums[0]);
+        arr2.push_back(nums[1]);
+        const int n = nums.size();
+        for (int i = 2; i < n; i++) {
+            if (arr1.back() > arr2.back())
+                arr1.push_back(nums[i]);
+            else arr2.push_back(nums[i]);
+        }
+        arr1.insert(arr1.end(), arr2.begin(), arr2.end());
+        return arr1;
+    }
+};
