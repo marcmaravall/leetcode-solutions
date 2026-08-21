1c96d049df5ed5d2d7824407abd6c2a1ff41d640
Marc Maravall Díez <marcmaravall@gmail.com>
2026-08-21 23:27:34 +0200

    Time: 4 ms (47.68%) | Memory: 13.2 MB (29.69%) - LeetSync

--- 47-permutations-ii/permutations-ii.cpp
+++ 47-permutations-ii/permutations-ii.cpp
@@ -1,24 +1,23 @@
 class Solution {
 public:
-    void backtrack(std::vector<int> nums, int i, std::vector<std::vector<int>>& res) {
+    void backtrack(std::vector<std::vector<int>>& res, std::vector<int> nums, int i) {
         const int n = nums.size();
-        if (i == n) {
+        if (i >= n-1) {
             res.push_back(nums);
             return;
         }
-
-        for (int k = i ; k < n; k++) {
-            if (i != k && nums[i] == nums[k])
+        for (int j = i; j < n; j++) {
+            if (j != i && nums[i] == nums[j])
                 continue;
-            std::swap(nums[i], nums[k]);
-            backtrack(nums, i+1, res);
+            std::swap(nums[i], nums[j]);
+            backtrack(res, nums, i+1);
         }
     }
 
     vector<vector<int>> permuteUnique(vector<int>& nums) {
         std::sort(nums.begin(), nums.end());
         std::vector<std::vector<int>> res;
-        backtrack(nums, 0, res);
+        backtrack(res, nums, 0);
         return res;
     }
 };
