7408a29befb0cdc0e26c8b7007dde0ff086044c3
Marc Maravall Díez <marcmaravall@gmail.com>
2026-08-25 00:41:06 +0200

    Time: 116 ms (30.29%) | Memory: 95.3 MB (49.85%) - LeetSync

--- 2002-stone-game-viii/stone-game-viii.cpp
+++ 2002-stone-game-viii/stone-game-viii.cpp
@@ -4,14 +4,12 @@ public:
         const int n = stones.size();
         std::vector<int> pref(n);
         pref[0] = stones[0];
-        for (int i = 1; i < n; i++) {
+        for (int i = 1; i < n; i++)
             pref[i] = stones[i]+pref[i-1];
-        }
         std::vector<int> dp(n);
         dp[n-1] = pref[n-1];
-        for (int i = n-2; i >= 1; i--) {
+        for (int i = n-2; i >= 1; i--)
             dp[i] = std::max(dp[i+1], pref[i]-dp[i+1]);
-        }
         return dp[1];
     }
 };
