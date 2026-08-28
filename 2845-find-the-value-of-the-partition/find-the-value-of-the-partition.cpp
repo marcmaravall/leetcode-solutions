4f434d3d9f4a974a027e235066294d4b4ccccdbf
Marc Maravall Díez <marcmaravall@gmail.com>
2026-08-28 20:19:36 +0200

    Time: 11 ms (99.71%) | Memory: 63.8 MB (12.35%) - LeetSync

--- 2845-find-the-value-of-the-partition/find-the-value-of-the-partition.cpp
+++ 2845-find-the-value-of-the-partition/find-the-value-of-the-partition.cpp
@@ -1,12 +1,11 @@
 class Solution {
 public:
     int findValueOfPartition(vector<int>& nums) {
-        std::sort(nums.begin(), nums.end(), std::greater<int>());
         int res = INT_MAX;
         const int n = nums.size();
-        for (int i = 0; i < n-1; i++) {
+        std::sort(nums.begin(), nums.end(), std::greater<int>());
+        for (int i = 0; i < n-1; i++)
             res = std::min(res, nums[i]-nums[i+1]);
-        }
         return res;
     }
 };
