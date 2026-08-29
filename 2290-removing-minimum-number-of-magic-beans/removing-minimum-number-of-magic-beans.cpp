5cf118dadcfb1628c97e3e01597a2102781d440d
Marc Maravall Díez <marcmaravall@gmail.com>
2026-08-29 20:25:46 +0200

    Time: 35 ms (95.65%) | Memory: 104.5 MB (96.98%) - LeetSync

--- 2290-removing-minimum-number-of-magic-beans/removing-minimum-number-of-magic-beans.cpp
+++ 2290-removing-minimum-number-of-magic-beans/removing-minimum-number-of-magic-beans.cpp
@@ -3,19 +3,10 @@ public:
     long long minimumRemoval(vector<int>& beans) {
         std::sort(beans.begin(), beans.end());
         const int n = beans.size();
-        std::vector<long long> prefix(n);
-        prefix[0] = beans[0];
-        for (int i = 1; i < n; i++)
-            prefix[i] = beans[i]+prefix[i-1];
+        long long total = std::accumulate(beans.begin(), beans.end(), 0LL);
         long long res = INT64_MAX;
         for (int i = 0; i < n; i++) {
-            long long curr = 0;
-            if (i != 0)
-                curr += prefix[i-1];
-            if (i < n-1) {
-                int s = n-i-1;
-                curr += (prefix[n-1]-prefix[i])-((long long)s*(long long)beans[i]);
-            }
+            long long curr = total - (long long)beans[i] * (n-i);
             res = std::min(res, curr);
         }
         return res;
