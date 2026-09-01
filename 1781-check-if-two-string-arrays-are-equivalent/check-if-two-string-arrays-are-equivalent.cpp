f5cc1accd2476f5be33c2be63e75be22a149cd15
Marc Maravall Díez <marcmaravall@gmail.com>
2026-09-01 23:44:51 +0200

    Time: 0 ms (100.00%) | Memory: 15 MB (36.10%) - LeetSync

---
+++ 1781-check-if-two-string-arrays-are-equivalent/check-if-two-string-arrays-are-equivalent.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool arrayStringsAreEqual(vector<string>& word1, vector<string>& word2) {
+        std::string w1, w2;
+        for (auto& s : word1)
+            w1 += s;
+        for (auto& s : word2)
+            w2 += s;
+        return w1 == w2;
+    }
+};
