cfb0355145af4f85bcc8bcfe213bdcb13e719b09
Marc Maravall Díez <marcmaravall@gmail.com>
2026-09-05 23:22:19 +0200

    Time: 0 ms (100.00%) | Memory: 9 MB (46.70%) - LeetSync

--- 1769-get-maximum-in-generated-array/get-maximum-in-generated-array.cpp
+++ 1769-get-maximum-in-generated-array/get-maximum-in-generated-array.cpp
@@ -6,11 +6,15 @@ public:
         std::vector<int> nums(n+1);
         nums[0] = 0;
         nums[1] = 1;
+        int res = 1;
         for (int i = 2; i <= n; i+=2) {
             nums[i] = nums[i/2];
-            if (i < n)
+            res = std::max(res, nums[i]);
+            if (i < n) {
                 nums[i+1] = nums[i/2]+nums[i/2+1];
+                res = std::max(res, nums[i+1]);
+            }
         }
-        return *std::max_element(nums.begin(), nums.end());
+        return res;
     }
 };
