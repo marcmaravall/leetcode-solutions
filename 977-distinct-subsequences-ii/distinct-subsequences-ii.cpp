15f8d93dafbf32ff9bd7b7581dd502ffdb45fe36
Marc Maravall Díez <marcmaravall@gmail.com>
2026-09-07 09:02:30 +0200

    Time: 0 ms (100.00%) | Memory: 8.9 MB (78.81%) - LeetSync

---
+++ 977-distinct-subsequences-ii/distinct-subsequences-ii.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int distinctSubseqII(string s) {
+        int res = 0;
+        int dp[26] = {0};
+        constexpr int mod = 1e9+7;
+        for (char c : s) {
+            c -= 'a';
+            int add = (res-dp[c]+mod) % mod;
+            dp[c] = res+1;
+            res = (dp[c]+add) % mod;
+        }
+        return res;
+    }
+};
