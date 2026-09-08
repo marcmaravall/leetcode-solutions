0befc07b014303c6fbe836ab78e38fc998326598
Marc Maravall Díez <marcmaravall@gmail.com>
2026-09-08 15:31:47 +0200

    Time: 0 ms (100.00%) | Memory: 20 MB (64.60%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
@@ -3,7 +3,7 @@ public:
     int maxProfit(vector<int>& prices) {
         const int n = prices.size();
         int res = 0;
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < n-1; i++) {
             if (i < n-1 && prices[i] > prices[i+1])
                 continue;
             int price = prices[i];
