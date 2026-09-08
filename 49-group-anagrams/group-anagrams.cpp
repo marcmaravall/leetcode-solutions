c7508ac7c14284c1da0b18b92f818fba3b602a80
Marc Maravall Díez <marcmaravall@gmail.com>
2026-09-08 15:16:12 +0200

    Time: 17 ms (53.20%) | Memory: 26 MB (52.90%) - LeetSync

--- 49-group-anagrams/group-anagrams.cpp
+++ 49-group-anagrams/group-anagrams.cpp
@@ -1,22 +1,11 @@
-struct ArrayHasher {
-    size_t operator()(const std::array<int, 26>& a) const {
-        size_t h = 0;
-        for (auto e : a) {
-            h ^= std::hash<int>{}(e) + 0x9e3779b9 + (h << 6) + (h >> 2);
-        }
-        return h;
-    }
-};
-
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        std::unordered_map<std::array<int, 26>, std::vector<std::string>, ArrayHasher> anagrams;
+        std::unordered_map<std::string, std::vector<std::string>> anagrams;
         for (auto& str : strs) {
-            std::array<int, 26> freq = {0};
-            for (char c : str)
-                freq[c-'a']++;
-            anagrams[freq].push_back(str);
+            std::string sorted = str;
+            std::sort(sorted.begin(), sorted.end());
+            anagrams[sorted].push_back(str);
         }
         std::vector<std::vector<std::string>> res;
         for (auto [letterFreq, vec] : anagrams)
