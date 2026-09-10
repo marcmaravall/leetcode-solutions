98e11deceed1388b4498517d2def51184fd01b56
Marc Maravall Díez <marcmaravall@gmail.com>
2026-09-10 20:19:50 +0200

    Time: 7 ms (71.93%) | Memory: 19.6 MB (23.71%) - LeetSync

--- 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.cpp
+++ 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.cpp
@@ -5,12 +5,12 @@ public:
         int res = n;
         for (int i = 0; i < n-1; i++) {
             int num = nums[i];
-            if (num == nums[i+1]) {
-                i++;
-                for (i; i < n-1 && num == nums[i+1]; i++) {
-                    nums[i] = INT_MIN;
-                    res--;
-                }
+            if (num != nums[i+1])
+                continue;
+            i++;
+            for (i; i < n-1 && num == nums[i+1]; i++) {
+                nums[i] = INT_MIN;
+                res--;
             }
         }
         nums.erase(std::remove(nums.begin(), nums.end(), INT_MIN), nums.end());
