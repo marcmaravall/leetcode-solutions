0d9b226882604fb60bdbb85c448cf62346cd60fd
Marc Maravall Díez <marcmaravall@gmail.com>
2026-09-10 20:19:11 +0200

    Time: 5 ms (76.07%) | Memory: 19.6 MB (59.57%) - LeetSync

---
+++ 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        const int n = nums.size();
+        int res = n;
+        for (int i = 0; i < n-1; i++) {
+            int num = nums[i];
+            if (num == nums[i+1]) {
+                i++;
+                for (i; i < n-1 && num == nums[i+1]; i++) {
+                    nums[i] = INT_MIN;
+                    res--;
+                }
+            }
+        }
+        nums.erase(std::remove(nums.begin(), nums.end(), INT_MIN), nums.end());
+        return res;
+    }
+};
