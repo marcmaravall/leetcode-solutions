0009d623b6618b1157bcc4a787ccd1ba5bd194eb
Marc Maravall Díez <marcmaravall@gmail.com>
2026-09-14 19:57:40 +0200

    Time: 24 ms (52.74%) | Memory: 29.3 MB (31.22%) - LeetSync

--- 3634-find-mirror-score-of-a-string/find-mirror-score-of-a-string.cpp
+++ 3634-find-mirror-score-of-a-string/find-mirror-score-of-a-string.cpp
@@ -3,15 +3,15 @@ public:
     long long calculateScore(string s) {
         long long res = 0;
         const int n = s.size();
-        std::unordered_map<char, std::stack<int>> stacks;
+        std::array<std::stack<int>, 26> stacks;
         for (int i = 0; i < n; i++) {
-            char mirror = 'z'-(s[i]-'a');
+            char mirror = 25-(s[i]-'a');
             if (!stacks[mirror].empty()) {
                 int j = stacks[mirror].top();
                 stacks[mirror].pop();
                 res += i-j;
             }
-            else stacks[s[i]].push(i);
+            else stacks[s[i]-'a'].push(i);
         }
         return res;
     }
