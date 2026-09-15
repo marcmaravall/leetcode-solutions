51405a2e1b99085780cd95cc33108a786f1b26a9
Marc Maravall Díez <marcmaravall@gmail.com>
2026-09-15 19:33:54 +0200

    Time: 5 ms (87.14%) | Memory: 31 MB (79.19%) - LeetSync

--- 4415-count-values-with-equally-spaced-occurrences-i/count-values-with-equally-spaced-occurrences-i.cpp
+++ 4415-count-values-with-equally-spaced-occurrences-i/count-values-with-equally-spaced-occurrences-i.cpp
@@ -3,15 +3,13 @@ public:
     int countSpecialIntegers(vector<int>& nums) {
         std::array<std::vector<int>, 101> positions;
         const int n = nums.size();
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++)
             positions[nums[i]].push_back(i);
-        }
         int res = 0;
         for (auto& p : positions) {
             if (p.size() != 3)
                 continue;
-            if (p[1]-p[0] == p[2]-p[1])
-                res++;
+            res += (p[1]-p[0] == p[2]-p[1]);
         }
         return res;
     }
