7d471f2b217a9fcfb199dc2097acd8453f610597
Marc Maravall Díez <marcmaravall@gmail.com>
2026-09-17 20:04:41 +0200

    Time: 0 ms (100.00%) | Memory: 14 MB (93.57%) - LeetSync

---
+++ 89-gray-code/gray-code.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    vector<int> grayCode(int n) {
+        int size = 1 << n;
+        std::vector<int> res(size);
+        for (int i = 0; i < size; i++) {
+            res[i] = i ^ (i >> 1);
+        }
+        return res;
+    }
+};
