9cbd26e9c8d7e7a19d721672db00de05b53c1f5d
Marc Maravall Díez <marcmaravall@gmail.com>
2026-09-18 22:56:10 +0200

    Time: 2 ms (3.98%) | Memory: 8.4 MB (18.19%) - LeetSync

--- 1341-split-a-string-in-balanced-strings/split-a-string-in-balanced-strings.cpp
+++ 1341-split-a-string-in-balanced-strings/split-a-string-in-balanced-strings.cpp
@@ -5,11 +5,8 @@ public:
         const int n = s.size();
         for (int i = 0; i < n; i++) {
             int freq[2] = {0};
-            while (i < n && (freq[0]*freq[1] == 0 || freq[0] != freq[1])) {
-                bool b = s[i] == 'R' ? 0 : 1;
-                freq[b]++;
-                i++;
-            }
+            while (i < n && (freq[0]*freq[1] == 0 || freq[0] != freq[1]))
+                freq[s[i++] == 'R' ? 0 : 1]++;
             i--;
             if (freq[0] == freq[1])
                 res++;
