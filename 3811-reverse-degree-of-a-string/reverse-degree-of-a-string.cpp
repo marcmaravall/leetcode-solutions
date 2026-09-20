af82ddf8afcedf5c09420cb3d1484287a8ebe272
Marc Maravall Díez <marcmaravall@gmail.com>
2026-09-20 21:38:52 +0200

    Time: 0 ms (100.00%) | Memory: 9.8 MB (47.66%) - LeetSync

--- 3811-reverse-degree-of-a-string/reverse-degree-of-a-string.cpp
+++ 3811-reverse-degree-of-a-string/reverse-degree-of-a-string.cpp
@@ -4,7 +4,7 @@ public:
         int res = 0;
         const int n = s.size();
         for (int i = 0; i < n; i++)
-            res += (i+1)*(26-(s[i]-'a'));
+            res += (i+1)*(26-s[i]+'a');
         return res;
     }
 };
