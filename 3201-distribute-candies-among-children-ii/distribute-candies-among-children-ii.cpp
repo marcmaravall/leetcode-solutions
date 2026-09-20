90a3f298258ce8de4cd7d0b718fcf8ebba095a18
Marc Maravall Díez <marcmaravall@gmail.com>
2026-09-20 21:48:16 +0200

    Time: 36 ms (11.04%) | Memory: 8.8 MB (97.66%) - LeetSync

--- 3201-distribute-candies-among-children-ii/distribute-candies-among-children-ii.cpp
+++ 3201-distribute-candies-among-children-ii/distribute-candies-among-children-ii.cpp
@@ -2,7 +2,8 @@ class Solution {
 public:
     long long distributeCandies(int n, int limit) {
         long long res = 0;
-        for (int i = 0; i <= std::min(n, limit); i++) {
+        const int s = std::min(n, limit);
+        for (int i = 0; i <= s; i++) {
             if (n-i > 2*limit)
                 continue;
             res += std::min(n-i, limit)-std::max(0, n-i-limit)+1;
