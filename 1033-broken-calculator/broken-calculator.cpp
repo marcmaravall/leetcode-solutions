5639b75347963401d4835a1c26bd6f6225c26e64
Marc Maravall Díez <marcmaravall@gmail.com>
2026-09-23 23:52:32 +0200

    Time: 0 ms (100.00%) | Memory: 7.8 MB (49.28%) - LeetSync

--- 1033-broken-calculator/broken-calculator.cpp
+++ 1033-broken-calculator/broken-calculator.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
-    int brokenCalc(int startValue, int target) {
+    int brokenCalc(int x, int y) {
         int res = 0;
-        while (startValue < target) {
-            res += target%2+1;
-            target = (target+1)/2;
+        while (x < y) {
+            res += y%2+1;
+            y = (y+1)/2;
         }
-        return res+startValue-target;
+        return res+x-y;
     }
 };
