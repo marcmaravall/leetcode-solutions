190d47c451e3379093751a4e0e35f16ee6584a35
Marc Maravall Díez <marcmaravall@gmail.com>
2026-09-26 20:32:27 +0200

    Time: 0 ms (100.00%) | Memory: 8 MB (35.01%) - LeetSync

--- 1806-count-of-matches-in-tournament/count-of-matches-in-tournament.cpp
+++ 1806-count-of-matches-in-tournament/count-of-matches-in-tournament.cpp
@@ -1,10 +1,6 @@
 class Solution {
 public:
     int numberOfMatches(int n) {
-        if (n <= 1)
-            return 0;
-        if (n % 2 == 0)
-            return numberOfMatches(n/2)+n/2;
-        return (n-1)/2+numberOfMatches((n-1)/2+1);
+        return n-1;
     }
 };
