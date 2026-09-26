7463456630b6f334acdf7b074204229b33b24d2e
Marc Maravall Díez <marcmaravall@gmail.com>
2026-09-26 21:06:22 +0200

    Time: 31 ms (43.37%) | Memory: 22.3 MB (22.89%) - LeetSync

---
+++ 1526-html-entity-parser/html-entity-parser.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    string entityParser(string text) {
+        std::string res = "";
+        const int n = text.size();
+        std::unordered_map<std::string, std::string> replace = {
+            {"&quot;", "\""},
+            {"&apos;", "\'"},
+            {"&amp;", "&"},
+            {"&gt;", ">"},
+            {"&lt;", "<"},
+            {"&frasl;", "/"},
+        };
+        for (int i = 0; i < n; i++) {
+            if (text[i] != '&') {
+                res += text[i];
+                continue;
+            }
+            std::string key = "";
+            key += text[i];
+            for (++i; i < n && text[i] != ';' && text[i] != '&'; i++) {
+                key += text[i];
+            }
+            if (i < n && text[i] == ';')
+                key += text[i];
+            const bool match = !replace[key].empty();
+            std::string val = !match ? key : replace[key]; 
+            if (text[i] == '&') {
+                i--;
+            }
+            res += val;
+        }
+        return res;
+    }
+};
